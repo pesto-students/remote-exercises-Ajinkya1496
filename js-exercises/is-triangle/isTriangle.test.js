0ba8451cf63d31eca3944a49f9355d86630a1e34
apple <apple@apples-MacBook-Pro.local>
2020-05-19 17:45:48 +0530

    added test cases

--- js-exercises/is-triangle/isTriangle.test.js
+++ js-exercises/is-triangle/isTriangle.test.js
@@ -8,4 +8,16 @@ describe('isTriangle', () => {
   it('should return false if triangle can not be formed given three lines', () => {
     expect(isTriangle(1, 4, 2)).toBe(false);
   });
+
+  it('should return false if triangle can not be formed given three lines', () => {
+    expect(isTriangle(7, 9, 2)).toBe(false);
+  });
+
+  it('should return true if triangle can be formed given three lines', () => {
+    expect(isTriangle(9, 11, 15)).toBe(true);
+  });
+
+  it('should return true if triangle can be formed given three lines', () => {
+    expect(isTriangle(13, 15, 17)).toBe(true);
+  });
 });
