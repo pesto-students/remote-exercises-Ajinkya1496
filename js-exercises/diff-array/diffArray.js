8379b28905dce64e16547aad959e9298b6f85f93
apple <apple@apples-MacBook-Pro.local>
2020-05-23 22:13:28 +0530

    modified the variable name

--- js-exercises/diff-array/diffArray.js
+++ js-exercises/diff-array/diffArray.js
@@ -1,9 +1,9 @@
 function diffArray(array1, array2) {
   //elements in the array 1 which are not there in the array 2
-  let difference1 = array1.filter(x => array2.indexOf(x) === -1);
+  let difference1 = array1.filter(element => array2.indexOf(element) === -1);
 
   //elements in the array 2 which are not there in the array 1
-  let difference2 = array2.filter(x => array1.indexOf(x) === -1);
+  let difference2 = array2.filter(element => array1.indexOf(element) === -1);
 
   let difference = difference1.concat(difference2);
 
