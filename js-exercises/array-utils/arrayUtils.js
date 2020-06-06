e7eca4dff6e70227bd70299c125165371fcae902
apple <apple@apples-MacBook-Pro.local>
2020-06-06 19:59:53 +0530

    modify array utilities functions

--- js-exercises/array-utils/arrayUtils.js
+++ js-exercises/array-utils/arrayUtils.js
@@ -1,57 +1,192 @@
-function forEach(array, callbackFn) {
-  if(!array.length) throw new TypeError('Passed argument is not an array');
-
-  if(typeof callbackFn !== "function") throw new TypeError('Requires a callback function to execute');
-
-  let arrayLength = array.length;
-  
-  for(let i = 0; i < arrayLength; i++) {
-    if(array[i]) {
-      callbackFn(array[i], i, array)  
-    }
+function ToObject(value) {
+  if(typeof value === undefined) {
+    throw new TypeError("this is undefined")
   }
-
-  return;
+  else if(value === null) {
+    throw new TypeError("this is null")
+  }
+  else if(typeof value === "boolean") {
+    return new Boolean(value);
+  }
+  else if(typeof value === "number") {
+    return new Number(value);
+  }
+  else if(typeof value === "string") {
+    return new String(value);
+  }
+  else if(typeof value === "symbol") {
+    return Symbol(value);
+  }
+  else if(typeof value === "object" && value !== null) {
+    return value;
+  }
 }
 
-function map(array, callbackFn) {
-  let resultingArray = [];
-  if(!array.length) throw new TypeError('Passed argument is not an array');
-
-  if(typeof callbackFn !== "function") throw new TypeError('Requires a callback function to execute');
-
-  let arrayLength = array.length;
-  
-  for(let i = 0; i < arrayLength; i++) {
-    if(array[i]) {
-      resultingArray.push(callbackFn(array[i], i, array));
-    }
-  }
-
-  return resultingArray;
+function HasProperty(O, Pk) {
+  return O.hasOwnProperty(Pk);
 }
 
-function filter(array, callbackFn) {
-  let resultingArray = [];
-  if(!array.length) throw new TypeError('Passed argument is not an array');
-
-  if(typeof callbackFn !== "function") throw new TypeError('Requires a callback function to execute');
-
-  let arrayLength = array.length;
-  
-  for(let i = 0; i < arrayLength; i++) {
-    if(array[i]) {
-      if(callbackFn(array[i], i, array))
-      resultingArray.push(array[i]);
+function ToBoolean(value) {
+  if(typeof value === undefined) {
+    return false;
+  }
+  else if(value === null) {
+    return false;
+  }
+  else if(typeof value === "boolean") {
+    return value;
+  }
+  else if(typeof value === "number") {
+    if(value === 0 || value === -0 || value === NaN) {
+      return false;
     }
+    return true;
+  }
+  else if(typeof value === "string") {
+    if(value.length === 0)  {
+      return false;
+    }
+    return true;
+  }
+  else if(typeof value === "symbol") {
+    return true;
+  }
+  else if(typeof value === "object" && value !== null) {
+    return true;
+  }
+}
+
+function forEach(callbackFn, thisArg) {
+  let O = ToObject(this);
+  let len = O['length'];
+  if(typeof callbackFn !== "function") {
+    throw new TypeError("Parameter passed as callback function is not a function");
   }
 
-  return resultingArray;
+  let T = thisArg !== undefined? thisArg: undefined;
+
+  let k = 0;
+
+  while(k < len) {
+    let Pk = k.toString();
+    let kPresent = HasProperty(O, Pk);
+    if(kPresent) {
+      let kValue = O[Pk];
+      callbackFn.call(T, kValue, k, O);
+    }
+
+    k += 1;
+  }
+
+  return undefined;
 }
+
+function map(callbackFn, thisArg) {
+  let O = ToObject(this);
+  let len = O['length'];
+
+  if(typeof callbackFn !== "function") {
+    throw new TypeError("Parameter passed as callback function is not a function");
+  }
+
+  let T = thisArg !== undefined? thisArg : undefined;
+
+  let A = new Array(len);
+
+  let k = 0;
+
+  while(k < len) {
+    let Pk = k.toString();
+    let kPresent = HasProperty(O, Pk);
+    if(kPresent) {
+      let kValue = O[Pk];
+      let mappedValue = callbackFn.call(T, kValue, k, O);
+      A[k] = mappedValue;
+    }
+    k += 1;
+  }
+
+  return A;
+}
+
+function filter(callbackFn, thisArg) {
+  let O = ToObject(this);
+  let len = O['length'];
+  
+  if(typeof callbackFn !== "function") {
+    throw new TypeError("Parameter passed as callback function is not a function");
+  }
+
+  let T = thisArg !== undefined? thisArg : undefined;
+
+  let A = new Array(0);
+
+  let k = 0;
+
+  let to = 0;
+
+  while(k < len) {
+    let Pk = k.toString();
+    let kPresent = HasProperty(O, Pk);
+    if(kPresent) {
+      let kValue = O[Pk];
+      let selected = ToBoolean(callbackFn.call(T, kValue, k, O));
+      if(selected === true) {
+        A[to] = kValue;
+        to += 1;
+      }
+    }
+    k += 1;
+  }
+
+  return A;
+}
+
+function reduce(callbackFn, thisArg) {
+  let O = ToObject(this);
+  let len = O['length'];
+
+  if(typeof callbackFn !== "function") {
+    throw new TypeError("Parameter passed as callback function is not a function");
+  }
+
+  if(len === 0) {
+    throw new TypeError("Length is 0");
+  }
+
+  let k = 0;
+  let accumulator = undefined;
+
+  if(arguments.length >= 2) {
+    accumulator = arguments[1];
+  }
+  else{
+    while(k < len && !(k in O)) {
+      k += 1;
+    }
+    if (k >= len) {
+      throw new TypeError('Reduce of empty array with no initial value');
+    }
+    accumulator = O[k++];
+  }
+
+  while(k < len) {
+    let Pk = k.toString();
+    let kPresent = HasProperty(O, Pk);
+    if(kPresent === true) {
+      let kValue = O[Pk];
+      accumulator = callbackFn.call(undefined, accumulator, kValue, O)
+    }
+    k += 1;
+  }
+
+  return accumulator;
+}
+
 
 export {
   forEach,
   map,
   filter,
-  // reduce,
+  reduce,
 }
--- js-exercises/array-utils/arrayUtils.test.js
+++ js-exercises/array-utils/arrayUtils.test.js
@@ -1,74 +1,80 @@
 // Write your own test cases.
-import { forEach, map, filter } from './arrayUtils';
+import { forEach, map, filter, reduce } from './arrayUtils';
 
 describe('Array utilities', () => {
     describe('Test Cases for forEach', () => {
         it('for each should return undefined', () => {
             expect(
-                forEach([1,2,3,4], function(value, index) {
+                forEach.call([1,2,3,4], function(value, index) {
                     // console.log(value);
                 })
             ).toBe(undefined);
         })
     
-        test('for each should give an error for a non-array value', () => {
-            expect( () => {
-                forEach(1, function(value, index) {})
-            }).toThrowError('Passed argument is not an array');
-        })
-    
         test('for each should give an error for a non-function value', () => {
             expect( () => {
-                forEach([1, 2, 3, 4], 'abcd')
-            }).toThrowError('Requires a callback function to execute');
+                forEach.call([1, 2, 3, 4], 'abcd')
+            }).toThrowError('Parameter passed as callback function is not a function');
         })  
     })
 
     describe('Test cases for map', () => {
         it('should return an array', () => {
             expect(
-               Array.isArray(map([1, 2, 3, 4], (element, index) => element * 2))
+               Array.isArray(map.call([1, 2, 3, 4], (element, index) => element * 2))
             ).toBe(true);
         })
 
         it('should return a correct array', () => {
             expect(
-               map([1, 2, 3, 4], (element, index) => element * 2)
+               map.call([1, 2, 3, 4], (element, index) => element * 2)
             ).toEqual([2, 4, 6, 8]);
 
             expect(
-                map([1, ,3, 4], (element, index) => element * 2)
-             ).toEqual([2, 6, 8]);
-        })
-
-        test('map should give an error for a non-array value', () => {
-            expect( () => {
-                forEach(1, function(value, index) {})
-            }).toThrowError('Passed argument is not an array');
+                map.call([1, ,3, 4], (element, index) => element * 2)
+             ).toEqual([2, undefined, 6, 8]);
         })
     
         test('map should give an error for a non-function value', () => {
             expect( () => {
-                forEach([1, 2, 3, 4], 'abcd')
-            }).toThrowError('Requires a callback function to execute');
+                map.call([1, 2, 3, 4], 'abcd')
+            }).toThrowError('Parameter passed as callback function is not a function');
         })
     })
 
     describe('Test cases for filter', () => {
         it('should return an array', () => {
             expect(
-               Array.isArray(filter([1, 2, 3, 4], (element, index) => element % 2 == 0))
+               Array.isArray(filter.call([1, 2, 3, 4], (element, index) => element % 2 == 0))
             ).toBe(true);
         })
 
         it('should return a correct array', () => {
             expect(
-               filter([1, 2, 3, 4], (element, index) => element % 2 !== 0)
+               filter.call([1, 2, 3, 4], (element, index) => element % 2 !== 0)
             ).toEqual([1,3]);
 
             expect(
-                filter(['1', , '3', 4], (element, index) => typeof element === "number")
+                filter.call(['1', , '3', 4], (element, index) => typeof element === "number")
              ).toEqual([4]);
+        })  
+    })
+
+    describe('Test cases for reduce', () => {
+        it('should return a number', () => {
+            expect(
+               typeof reduce.call([null, , 3, 4], (accumulator, currentValue) => accumulator + currentValue) === "number"
+            ).toBe(true);
+        })
+
+        it('should return a correct result', () => {
+            expect(
+               reduce.call([1, 2, 3, 4], (accumulator, currentValue) => accumulator * currentValue)
+            ).toEqual(24);
+
+            expect(
+                reduce.call([null, undefined, 102, 408], (accumulator, currentValue) => accumulator * currentValue)
+             ).toEqual(NaN);
         })  
     })
 })
